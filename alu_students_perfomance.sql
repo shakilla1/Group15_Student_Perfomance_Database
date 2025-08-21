fcf8bdb42753489c950598247b42560c222a04e6
lisaineza <l.ineza@alustudent.com>
2025-08-21 21:24:07 +0200

    student values

--- alu_students_perfomance.sql
+++ alu_students_perfomance.sql
@@ -19,3 +19,22 @@ CREATE TABLE python_grades (
     grade_obtained DECIMAL(5, 2),
     FOREIGN KEY (student_id) REFERENCES students(student_id) 
 )
+
+Insert the sample data 
+-- Insert Students
+INSERT INTO students VALUES
+(1, 'Alice Mugenzi', 2023),
+(2, 'Brian Uwimana', 2023),
+(3, 'Cynthia Niyonsaba', 2024),
+(4, 'David Habimana', 2023),
+(5, 'Elise Uwase', 2024),
+(6, 'Frank Nkurunziza', 2023),
+(7, 'Grace Mukamana', 2024),
+(8, 'Henry Mugisha', 2023),
+(9, 'Irene Ishimwe', 2024),
+(10, 'John Rukundo', 2023),
+(11, 'Karen Uwera', 2023),
+(12, 'Louis Nsengiyumva', 2024),
+(13, 'Maria Nshuti', 2023),
+(14, 'Peter Kamanzi', 2024),
+(15, 'Queen Umutoni', 2023);
