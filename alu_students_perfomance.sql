38744fb7702c5b1e85b1f530df39d426f9d4d9a2
shakilla1 <s.uwamahoro1@alustudent.com>
2025-08-21 20:28:36 +0200

    add my file to repo

---
+++ alu_students_perfomance.sql
@@ -0,0 +1,21 @@
+CREATE TABLE students (
+    student_id INT PRIMARY KEY,
+    student_name VARCHAR(100) NOT NULL,
+    intake_year YEAR NOT NULL,
+);
+
+CREATE TABLE linux_grades (
+    course_id INT,
+    course_name VARCHAR(50) NOT NULL,
+    student_id INT,
+    grade_obtained DECIMAL(5, 2),
+    FOREIGN KEY (student_id) REFERENCES students(student_id)
+);
+
+CREATE TABLE python_grades (
+    course_id INT,
+    course_name VARCHAR(50) NOT NULL,
+    student_id INT,
+    grade_obtained DECIMAL(5, 2),
+    FOREIGN KEY (student_id) REFERENCES students(student_id) 
+)
